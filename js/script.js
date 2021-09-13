45b3ec72ea03352d58491ef495053b453b28739e
maximlesy <maximlesy@gmail.com>
2021-09-13 23:00:29 +0200

    Update conventions

--- js/script.js
+++ js/script.js
@@ -1,138 +1,137 @@
-'use strict';
+"use strict";
 
 window.addEventListener("load", initialize);
 
 //anonieme functie
-//window.addEventListener("load", function () { alert("HTML ingeladen!"); });
+//window.addEventListener("load", function () { 
+//  alert("HTML ingeladen!"); 
+//});
 
 function initialize() {
 
-  const button = document.getElementById("example");
+  const btnAnonymous = document.getElementById("anonymous-example");
 
-  button.addEventListener("click", function () {
+  btnAnonymous.addEventListener("click", function () {
     alert("Ik ben helemaal geheim!");
   });
-
-
-
 
   // ############################################
   // functies die geen resultaat terugkrijgen
   // ############################################
 
   //aanroepen van een function
-  doeIets();
+  logInfo();
 
   //meermaals aanroepen van een function
   for (let i = 0; i < 10; i++) {
-    doeIets();
+    logInfo();
   }
 
   //argumenten doorgeven aan een functie
   for (let i = 0; i < 10; i++) {
-    doeNogIets(`Aanroep ${i}`);
+    logMessage(`Aanroep ${i}`);
   }
 
-  oppervlak(5, 3);
-  oppervlak('test', 2);
-  oppervlak(5);
+  logSurface(5, 3);
+  logSurface("test", 2);
+  logSurface(5);
 
   // ############################################
   // functies die wel een resultaat terugkrijgen
   // ############################################
-  
+
   //retour van een enkel resultaat
-  const getal = Trippel(3);
-  console.log(getal);
-  
-  const getal2 = Trippel("hallo!");
-  console.warn(getal2);
-  
+  const trippledNumber = tripple(3);
+  console.log(trippledNumber);
+
+  const trippledString = tripple("hallo!");
+  console.warn(trippledString);
+
   //retour van meerdere resultaten (als object)
-  const berekendeoppervlakte = oppervlak2(2, 3);
-  if (!berekendeoppervlakte.foutingave)
-  console.log(`Berekende oppervlakte is ${berekendeoppervlakte.vlakte}`);
+  const basketballCourt = getSurface(28, 15);
+  if (!basketballCourt.isValid)
+    console.log(`Een basketveld heeft een oppervlakte van ${basketballCourt.result} m².`);
   else
-  console.warn('Foutief gebruik van functie oppervlak(h,b)');
-  
+    console.warn("Foutief gebruik van functie getSurface(height, width)");
+
   // ############################################
   // functies als variabelen
   // ############################################
 
-
   // variabele als functie
-  const functieAlsVariabele = function(iets) {
-    console.log(iets);
-  } 
-  functieAlsVariabele("Functie als variabele werkt!");
+  const logSentence = function (sentence) {
+    console.log(sentence);
+  };
+
+  logSentence("Functie als variabele werkt!");
 
   // ############################################
   // arrow functions
   // ############################################
-  
-  //met arugumenten ('a' ~ 2):
-  const double = a => a * 2;
 
-  const verdubbel = a => (a *2);
-  console.log(verdubbel(8));
+  //met arugumenten ("number" ~ 2):
+  const double = number => number * 2;
+  console.log(double(8));
 
   //zonder argumenten:
-  const consoleZin = () => {
-    console.log('Een zin in de console'); 
-    console.log('Een 2de zin');
+  const logSentences = () => {
+    console.log("Een zin in de console");
+    console.log("Een 2de zin");
   };
-  consoleZin();
+
+  logSentences();
 
 }
 
 // paramaterloze functie
-function doeIets() {
-  console.info("Er is iets gebeurd!");
+function logInfo() {
+  console.info("Er werd iets gelogged!");
 }
 
 //functie met een parameter
-function doeNogIets(zin) {
-  console.info(zin);
+function logMessage(message) {
+  console.info(message);
 }
 
 //functie met meerdere parameters.
 //mbv. de arguments property wordt gecheckt of alle parameters meegegeven werden
-function oppervlak(hoogte, breedte) {
-  if (arguments.length == 2) {
+function logSurface(height, width) {
+  if (arguments.length === 2) {
 
-    if (!isNaN(hoogte) && !isNaN(breedte))
-      console.log(`De oppervlakte is ${hoogte} * ${breedte} = ${hoogte * breedte}`);
+    if (!isNaN(height) && !isNaN(width))
+      console.log(`De oppervlakte is ${height} * ${width} = ${height * width}`);
 
     else
-      console.warn('Functie oppervlak(h,b) werkt enkel met getallen');
+      console.warn('Functie logSurface(width, height) werkt enkel met getallen');
 
   }
 
   else
-    console.warn("Functie oppervlak(h,b) verkeerd gebruikt, er dienen 2 parameters te worden meegegeven met deze functie");
+    console.warn("Functie logSurface(width, height) verkeerd gebruikt, er dienen 2 parameters te worden meegegeven met deze functie");
 
 }
 
-function oppervlak2(hoogte, breedte) {
-  let oppervlak = 0;
-  let fout = true;
-  if ((arguments.length != 2) || (isNaN(hoogte) || (isNaN(breedte)))) {
-    fout = true;
+function getSurface(height, width) {
+  let surface = 0;
+  let hasError = true;
+
+  if ((arguments.length !== 2) || (isNaN(height) || (isNaN(width)))) {
+    hasError = true;
   }
   else {
-    fout = false;
-    oppervlak = hoogte * breedte;
+    hasError = false;
+    surface = height * width;
   }
 
   // Resultaatobject meegeven
   return {
-    'vlakte': oppervlak,
-    'foutingave': fout
+    result: surface,
+    isValid: hasError
   };
 }
 
 //waarde retourneren
-function Trippel(getal) {
-  if (!isNaN(getal)) return 3 * getal;
-  else return 'Foutief gebruik, getal ingeven!';
+function tripple(number) {
+  if (!isNaN(number)) return 3 * number;
+  else return "Foutief gebruik, getal ingeven!";
 }
